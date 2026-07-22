36e9b7ff884fccec5734c40a9e4c9e420c3ba514
Ahmad Yousuf Jan <ahmadgigyani7@gmail.com>
2026-07-22 22:54:59 +0500

    Add files via upload

---
+++ Numpy8.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a1 = np.array([[1,2],[3,4]])
+a2 = np.array([[5,6],[7,8]])
+
+print(a1+a2)
+
+print(a1-a2)
+
+print(a1*a2)
+
+print(np.matmul(a1,a2))
+
+print(a1)
+print(a1.T)
+
+print(np.dot(a1,a2))
